e2705384266079d7d7285ae93ab83b48c6eed4c6
rantiiarn <rantir570@gmail.com>
2023-01-20 17:38:10 +0700

    ea

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container-fluid">
+      <div>
+
+ 
+
+      
+    </div>
+      <div class="row" >
+        <div class="col-lg-6" v-for="data in datas" >      
+          <br> <div class="card" style="width: 18rem;" pading >
+      <div class="card-body"  >
+    <h5 class="card-title">{{ data.isi }}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">{{ data.penulis }}</h6>
+    <a href="http://twitter.com/share?text= + data.isi" target="_blank" class="text-primary  text-decoration-none ">twitter</a>
+  
+  </div>
+  </div>
+  <br>
+ <button @click="getsatu" class="btn btn-primary">Generate</button>   
+</div>
+  
+  </div>
+</div>
+</template>
+<script setup>
+const supabase = useSupabaseClient()
+const datas = ref([])
+async function getData() {
+  const { data, eror } = await supabase
+  .from("quotes")
+  .select()
+  .limit(1)
+ 
+
+  datas.value = data
+  console.log(data)
+}
+async function getsatu(){
+  const{data,error}= await supabase
+  .from("random_quotes")
+  .select()
+  .limit(1)
+
+  datas.value=data
+}
+onMounted(()=> {
+  getData()
+})
+</script>
---
+++ pages/tambah.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="">
+
+  
+  <div class="col-md-6 m-auto mt-5 card p-3 ">
+    <h3 class="text-dark text-center fw-bolder mb-3">IsiQuotes</h3>
+<form @submit.prevent="simpan">
+<div class="row mb-3">
+  <label for="colFormLabel" class="col-sm-2 col-form-label">penulis</label>
+  <div class="col-sm-10">
+    <input type="text" class="form-control  " id="qoutes" placeholder="penulis" v-model="penulis">
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="colFormLabel" class="col-sm-2 col-form-label">Your Quotes</label>
+  <div class="col-sm-10">
+    <textarea type="text" class="form-control" id="colFormLabel" placeholder="" v-model="isi"></textarea>
+  </div>
+
+</div>
+
+<button type="submit" class="btn btn-primary ">sumbit</button>
+</form>
+</div> 
+</div>
+</template>
+
+
+<script setup>
+
+
+const supabase = useSupabaseClient()
+
+const isi = ref()
+const penulis = ref()
+async function simpan() {
+  await supabase
+  .from("quotes")
+  .insert({
+    penulis: penulis.value,
+    isi: isi.value,
+
+  })
+  navigateTo("/")
+}
+</script>
