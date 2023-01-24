b42ae6b301938d40dfffab2d7892063a69a161e8
rantiiarn <rantir570@gmail.com>
2023-01-24 11:47:30 +0700

    add tambah

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,20 @@
 <template>
-    <div class="container-fluid">
-      <div>
-
- 
-
-      
-    </div>
+    <div class="container m-auto">
+   
       <div class="row" >
-        <div class="col-lg-6" v-for="data in datas" >      
-          <br> <div class="card" style="width: 18rem;" pading >
-      <div class="card-body"  >
+        <div class="col-lg-6 m-auto mt-5" v-for="data in datas" >      
+          <br> <div class="card" style="width: 18rem;"  >
+      <div class="card-body" >
+        <h3 class="text-dark text-center fw-bolder">Quotes</h3>
     <h5 class="card-title">{{ data.isi }}</h5>
     <h6 class="card-subtitle mb-2 text-muted">{{ data.penulis }}</h6>
-    <a href="http://twitter.com/share?text= + data.isi" target="_blank" class="text-primary  text-decoration-none ">twitter</a>
+    <a href="http://twitter.com/share?text= + data.isi" target="_blank" class="text-primary  text-decoration-none ">twitter</a><br>
   
-  </div>
+    <button @click="getsatu" class="btn btn-primary">Generate</button>
+    <NuxtLink to="/tambah" class="btn btn-success mx-3">Tambah</NuxtLink>
+  </div> 
   </div>
   <br>
- <button @click="getsatu" class="btn btn-primary">Generate</button>   
 </div>
   
   </div>
